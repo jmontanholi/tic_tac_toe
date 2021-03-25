521f3a8c627d89517f3f4ef3a683c4c34eb54955
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-25 18:21:43 -0300

    Fix win condition logic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -25,21 +25,10 @@ def design_board(slots)
   puts '+---+---+---+'
 end
 
-def check_win(player_arr)
-  winning_numbers = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 5, 9],
-  [3, 5, 7],
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 6, 9]
-  ]
-
-  winning_numbers.each do |array|
-    control = 0
-    array.each { |num| control += 1 if player_arr.include?(num)  }
-    control.zero?
+def check_win(player_arr, winning_array)
+  control = false
+  winning_array.each do |array|
+    control = true if array.all? { |num| player_arr.include?(num)  }
   end
+  return control
 end
