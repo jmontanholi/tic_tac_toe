16cb292ee667106ba7f50387f41c1900326a350a
pountzas <nikos@pountzas.gr>
2021-03-27 01:35:38 +0200

    add display last condition before win

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -34,32 +34,34 @@ class Game
     tie = false
     until tie
       # Make board First time.
-      Board.design_board(slots)
+      design_board(slots)
       # Define remaining_slots variable.
       remaining_slots = slots.select { |item| item.is_a? Integer }
       # Player 1 turn.
       player_turn(slots, @player1.name, 'X', remaining_slots, player1_arr)
       sleep 1
       # Check If player 1 wins with his move.
+      design_board(slots)
       break if winner(@player1, player1_arr, winning_numbers)
 
       # Clear board so winner message does not stay in the screen in a tie.
-      Board.design_board(slots)
+      design_board(slots)
       # Check if it is a tie.
       break if tie(slots)
 
       # Redefine remaining_slots to give players their real options.
       remaining_slots = slots.select { |item| item.is_a? Integer }
       # Clear board so tie message does not stick in the screen.
-      Board.design_board(slots)
+      design_board(slots)
       # Player 2 turn.
       player_turn(slots, @player2.name, 'O', remaining_slots, player2_arr)
       sleep 1
       # Check If player 2 wins with his move.
+      design_board(slots)
       break if winner(@player2, player2_arr, winning_numbers)
 
       # Clear board so winner message does not stay in the screen in a tie.
-      Board.design_board(slots)
+      design_board(slots)
       # Check if it is a tie
       break if tie(slots)
     end
