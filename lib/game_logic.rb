9b80b9f19090cb40abc06ae4a52fb86ac1b29525
jmontanholi <jonunes9326@hotmail.com.br>
2021-03-26 09:27:40 -0300

    Fix code reviewer requested changes

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -6,23 +6,19 @@ class Player
   end
 end
 
-def greeting
-  puts 'TIC TAC TOE GAME'
-  puts 'TO START PRESS ENTER'
-  gets.chomp
-end
+class Board
+  def self.design_board(slots)
+    system('clear')
+    system('cls')
 
-def design_board(slots)
-  system('clear')
-  system('cls')
-
-  puts '+---+---+---+'
-  puts "| #{slots[0]} | #{slots[1]} | #{slots[2]} |"
-  puts '+---+---+---+'
-  puts "| #{slots[3]} | #{slots[4]} | #{slots[5]} |"
-  puts '+---+---+---+'
-  puts "| #{slots[6]} | #{slots[7]} | #{slots[8]} |"
-  puts '+---+---+---+'
+    puts '+---+---+---+'
+    puts "| #{slots[0]} | #{slots[1]} | #{slots[2]} |"
+    puts '+---+---+---+'
+    puts "| #{slots[3]} | #{slots[4]} | #{slots[5]} |"
+    puts '+---+---+---+'
+    puts "| #{slots[6]} | #{slots[7]} | #{slots[8]} |"
+    puts '+---+---+---+'
+  end
 end
 
 def check_win(player_arr, winning_array)
